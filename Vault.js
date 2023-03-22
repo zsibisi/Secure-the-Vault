e29abf7d4b8c2a3e677fc1e292d227c9172834a8
Zinhle Sibisi <107902020+zsibisi@users.noreply.github.com>
2023-03-21 17:16:08 -0700

    Create Vault.js

---
+++ Vault.js
@@ -0,0 +1,24 @@
+// Step 1
+let message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
+
+// Step 2
+const combination = "10 - 40 - 39";
+
+// Step 3 and Step 4
+let code1 = 5 + 5; // using addition
+let code2 = 5 * 8; // using multiplication
+let code3 = 80 - 41; // using subtraction
+let total = code1 - code2 - code3
+
+// Step 5
+// code1 = 10, code2 = 40, code3 = 39 and total = code1 - code2 - code3
+
+// Step 5 
+//let total = code1 - code2 -code3
+
+//step 6
+let finalMessage = message + combination + "\nCode 1: " + code1 + "\nCode 2: " + code2 + "\nCode 3: " + code3 +"\ntotal: " + total;
+
+
+// Step 7
+alert(finalMessage); 
